9116c41f7e7463ec5bc83b38203ba1ea20a32515
Bhavik Jain <bhavikjain358@gmail.com>
2016-11-09 00:00:36 -0500

    Changes made to the components Sidebar, Navbar, and index.html

--- app/app.js
+++ app/app.js
@@ -1,11 +1,18 @@
-/* Empty for now. :) */
 import React from 'react';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import ReactDOM from 'react-dom';
+import { IndexRoute, Router, Route, hashHistory } from 'react-router';
 
-//import { IndexRoute, Router, Route, hashHistory } from 'react-router';
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <Sidebar />
+        <Navbar />
+      </div>
+    );
+  }
+}
 
-ReactDOM.render((
-  <Navbar />
-),document.getElementById('container'));
+ReactDOM.render(<App />, document.getElementById('wrapper'));
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,21 +2,27 @@ import React from 'react';
 
 export default class Navbar extends React.Component{
   render(){
+
+    const proPicStyle = {
+      width: '60%',
+      height: '60%',
+    };
+
     return (
       <div>
-        <nav class="navbar navbar-light bg-faded" id="home_page_navbar">
-          <div class="col-lg-3"></div>
-          <div class="col-lg-6" id="title">
+        <nav className="navbar navbar-light bg-faded" id="home_page_navbar">
+          <div className="col-lg-3"></div>
+          <div className="col-lg-6" id="title">
                <h3>Welcome to</h3>
                <h1>SoundRoom</h1>
            </div>
-           <div class="col-lg-2"></div>
-             <div class="col-lg-1">
-             <ul class="nav navbar-nav">
-             <li class="dropdown top-dropdown">
-                 <img src="../../build/img/profile_pic.jpg" alt="Join Room" class="img-responsive center-block img-circle" style="width:60%;height:60%;"/>
-               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i id="user_icon"></i><span class="caret"></span></a>
-               <ul class="dropdown-menu">
+           <div className="col-lg-2"></div>
+             <div className="col-lg-1">
+             <ul className="nav navbar-nav">
+             <li className="dropdown top-dropdown">
+                <img src="/img/profile_pic.jpg" alt="Join Room" className="img-responsive center-block img-circle" style={proPicStyle}/>
+               <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i id="user_icon"></i><span className="caret"></span></a>
+               <ul className="dropdown-menu">
                  <li><a href="/account_info.html">Account Info</a></li>
                  <li><a href="#">Sign out</a></li>
                </ul>
