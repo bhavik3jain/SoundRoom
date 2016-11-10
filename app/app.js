16712f6ba870ee14cd1fcd3c575751c2058df5d8
Aarsh Patel <aarshpatel@umass.edu>
2016-11-09 22:44:54 -0500

    Another edit to app.js file

--- app/app.js
+++ app/app.js
@@ -6,10 +6,6 @@ import Playlists from './components/Playlists';
 import ReactDOM from 'react-dom';
 import { IndexRoute, Router, Route, hashHistory } from 'react-router';
 
-console.log(Navbar);
-//import { IndexRoute, Router, Route, hashHistory } from 'react-router';
-ReactDOM.render(<Navbar />,document.getElementById('container'));
-
 class App extends React.Component{
   render(){
     return (
