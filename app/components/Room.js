8197b60012212bcbcd0acf2348b6ab053c53dda2
Lynn Samson <lasamson@umass.edu>
2016-11-13 00:44:13 -0500

    Change Room component name

--- app/components/Room.js
+++ app/components/Room.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Join extends React.Component{
+export default class Room extends React.Component{
     render() {
         return (
             <div>
