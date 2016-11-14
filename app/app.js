114e32a8cef6f0f46ec6fc74a85ba755b7e1664d
Bhavik Jain <bhavikjain358@gmail.com>
2016-11-14 11:32:52 -0500

    Changes made to react router

--- app/app.js
+++ app/app.js
@@ -70,7 +70,7 @@ ReactDOM.render((
       <Route path="account" components={{content: AccountPage}}/>
       <Route path="createroom" components={{content: CreateRoom}}/>
       <Route path="joinroom" components={{content: JoinRoom}}/>
-      <Route path="playlists/:playlistData" components={{content: Playlists}}/>
+      <Route path="playlists" components={{content: Playlists}}/>
       <Route path="room/:roomId" components={{content: Room}}/>
     </Route>
   </Router>
