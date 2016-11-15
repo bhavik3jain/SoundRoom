eda8b97a5e9c86062a8aaa84e44024c5194a87a7
Aarsh Patel <aarshpatel@umass.edu>
2016-11-15 14:51:39 -0500

    Fixed the validate room function

--- app/components/JoinRoom.js
+++ app/components/JoinRoom.js
@@ -18,17 +18,16 @@ export default class JoinRoom extends React.Component{
   }
 
   validateRoom(e){
-
     e.preventDefault();
+    var roomsIds = [];
     var rooms = getRoomIds();
-    rooms.forEach(function(room){
-      if (room._id == document.getElementById("userInput").value){
 
-        roomNumber = room._id;
-
-      }
-    });
-
+    for (var room in rooms) {
+      var input_room_number = document.getElementById("userInput").value;
+      if(rooms[room]._id == input_room_number){
+        console.log("Allowing user to join the room " + input_room_number);
+      }     
+    }
   }
 
 
@@ -41,10 +40,10 @@ export default class JoinRoom extends React.Component{
                      <div id="join_room_panel" className="panel panel-default panel-joinroom">
                        <div className="panel-body">
                          <h2 id="join_room_text">Join a Room</h2>
-                         <input type="text" className="form-control" id="userInput" onChange={(e)=>this.validateRoom(e)}
+                         <input type="text" className="form-control" id="userInput" 
                              placeholder="Enter Room Number" />
                            <Link to={{pathname:"room",query:{roomId:roomNumber}}}>
-                               <button type = "button" className="btn btn-default" id="btn1" >Join</button>
+                               <button type = "button" className="btn btn-default" id="btn1" onClick={(e)=>this.validateRoom(e)}>Join</button>
                            </Link>
                            <Link to="/">
                              <button type = "button" className="btn btn-default" id="btn2" >Back</button>
