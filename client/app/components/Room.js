f4287fc0451c923f4a5579ad08a4987289031123
Justin Martinelli <justinmartinelli1@gmail.com>
2016-11-29 20:52:58 -0500

    fix accessCode visual in RoomPage

--- client/app/components/Room.js
+++ client/app/components/Room.js
@@ -32,7 +32,7 @@ export default class Room extends React.Component{
                   </div>
                       </div>
                       <div id='access-code' className="col-md-3">
-                        <h3> ACCESS CODE: {this.state.currentRoomId}</h3>
+                        <center><h3> ACCESS CODE:</h3><h3 className = 'code'>{this.state.currentRoomId}</h3></center>
                         <RoomParticipants currentRoomId={this.state.currentRoomId} />
                       </div>
                     </div>
