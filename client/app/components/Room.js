c022deadf3c769b011cd1ce92190049746f09724
Justin Martinelli <justinmartinelli1@gmail.com>
2016-11-29 19:29:15 -0500

    add some accessCode components

--- client/app/components/Room.js
+++ client/app/components/Room.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import RoomPlaylist from './RoomPlaylist';
 import RoomParticipants from './RoomParticipants';
-import {getRoomData, getSongsData, getUserIds} from '../server';
+import {getMakeId, getRoomData, getSongsData, getUserIds} from '../server';
 
 
 export default class Room extends React.Component{
+
 
     constructor(props) {
       super(props);
       this.state = {
           currentRoomId: this.props.location.query.roomId,
-          currentUser: this.props.location.query.user_logged_in,
+          currentUser: this.props.location.query.user_logged_in
       }
     }
-
 
     componentWillMount() {
       this.setState({currentRoomId: this.state.currentRoomId});
     }
 
     render() {
+        console.log(this.props.location.query);
         return (
             <div>
             <div>
@@ -31,6 +32,7 @@ export default class Room extends React.Component{
                   </div>
                       </div>
                       <div className="col-md-3">
+                        <p>Access Code:</p>{this.state.currentRoomId}
                         <RoomParticipants currentRoomId={this.state.currentRoomId} />
                       </div>
                     </div>
