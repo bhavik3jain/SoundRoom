5de8de0454870bf12dbf2fedd7ec26a2c31614f3
ronitarora <rarora@umass.edu>
2016-12-05 17:05:29 -0500

    Added a link to the loginpage

--- client/app/app.js
+++ client/app/app.js
@@ -8,7 +8,7 @@ import JoinRoom from './components/JoinRoom';
 import AccountInfo from './components/AccountInfo';
 import Room from './components/Room';
 import ErrorBanner from './components/errorbanner';
-
+import loginpage from './components/loginpage';
 import ReactDOM from 'react-dom';
 import { IndexRoute, Router, Route, browserHistory } from 'react-router';
 
@@ -42,6 +42,15 @@ class PlaylistPage extends React.Component{
     return (
       <div>
         <Playlists />
+      </div>
+    )
+  }
+}
+class loginpage extends React.component{
+  render(){
+    return(
+      <div>
+      <loginpage />
       </div>
     )
   }
@@ -81,6 +90,7 @@ ReactDOM.render((
       <Route path="playlists" components={{content: Playlists}} />
       <Route path="room" components={{content: Room}}/>
       <Route path="homepage" components={{content: Homepage}}
+      <Route path="loginpage" components={{content:loginpage}}/>
     </Route>
   </Router>
 ), document.getElementById('wrapper'))
---
+++ client/app/components/loginpage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {getRoomIds, joinRoom} from '../server';
+// import {writeDocument} from '../database';
+import {Link, browserHistory} from 'react-router';
+import ErrorBanner from './errorbanner'
+
+
+
+export default class loginpage extends React.Component{
+
+
+
+  render() {
+        return (
+            <div>
+                <div className = "col-md-4"></div>
+                 <div className = "col-md-4">
+                     <div id="login_room_panel" className="panel panel-default panel-joinroom">
+                       <div className="panel-body">
+                         <h2 id="login_room_text">Login</h2>
+                         <input type="text" className="form-control" id="userInput"
+                             placeholder="Enter user ID to login " />
+                           <center>
+                                <Link to={{pathname:'Homepage',query:{user_logged_in:document.getElementById("userInput").value}}}>
+                            <button type = "button" className="btn btn-default" id="btn1">Login</button>
+                            </Link>
+                           </center>
+                       </div>
+                     </div>
+                   </div>
+                 <div className = "col-md-4"></div>
+            </div>
+        );
+    }
+}
