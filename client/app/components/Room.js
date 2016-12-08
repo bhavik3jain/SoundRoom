72d70959089717b0643ed0390e15a48842eaea0e
Aarsh Patel <aarshpatel@umass.edu>
2016-12-08 18:23:00 -0500

    AccountInfo

--- client/app/components/Room.js
+++ client/app/components/Room.js
@@ -131,7 +131,7 @@ export default class Room extends React.Component{
                                     <input type="text" id="phone_number_to_share" className="form-control" placeholder="xxxxxxxxxx" aria-describedby="basic-addon1" />
                                 </div>
                                   <br />
-                                  <button type="button" value="sub" name="sub" className="btn btn-primary" onClick={(e) => this.shareAccessCode(e)}><i className="fa fa-share"></i> Share!</button>
+                                  <button type="button" value="sub" name="sub" className="btn btn-default" onClick={(e) => this.shareAccessCode(e)}><i className="fa fa-share"></i> Share!</button>
                             </form>
                     </div>
                     <div className="modal-footer">
