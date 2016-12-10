d11d52b4bc313f86374165135bb43b3bf35cc8d4
Aarsh Patel <aarshpatel@umass.edu>
2016-12-10 00:44:38 -0500

    Testing for server

--- client/app/app.js
+++ client/app/app.js
@@ -97,14 +97,13 @@ class App extends React.Component{
 
 ReactDOM.render((
   <Router history={browserHistory}>
-    <Route path="/" component={LandingPage}>
+    <Route path="/" component={App}>
       <Route path="account" components={{content: AccountPage}}/>
       <Route path="createroom" components={{content: CreateRoom}}/>
       <Route path="joinroom" components={{content: JoinRoom}}/>
       <Route path="playlists" components={{content: Playlists}} />
       <Route path="room" components={{content: Room}}/>
       <Route path="homepage" components={{content: Homepage}}/>
-      <Route path="login" components={{content: LoginPage}}/>
     </Route>
   </Router>
 ), document.getElementById('wrapper'))
